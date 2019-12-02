37d8d192e2b41c2059634eb6d60c7239e841fd03
Douglas-Marques <douglasmarques09@gmail.com>
2019-12-02 02:46:44 -0300

    Updates

---
+++ precache-manifest.86aa74bd69f5377666f261997c1181e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944578189111cce21716d6c65dd66095",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "07011e047e773f2dc9f4",
+    "url": "/home/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "e4ca150e73c56813b55d",
+    "url": "/home/static/css/main.17674f0d.chunk.css"
+  },
+  {
+    "revision": "07011e047e773f2dc9f4",
+    "url": "/home/static/js/2.afaba035.chunk.js"
+  },
+  {
+    "revision": "e4ca150e73c56813b55d",
+    "url": "/home/static/js/main.c6c10bca.chunk.js"
+  },
+  {
+    "revision": "65e359858aabd8315332",
+    "url": "/home/static/js/runtime-main.70bc6f2e.js"
+  },
+  {
+    "revision": "4147f3fd5f2e9b5eda890a67f4cd3f4f",
+    "url": "/home/static/media/Brandon_light.4147f3fd.otf"
+  },
+  {
+    "revision": "eb722da39f5f8058b6a8ab5a0973249f",
+    "url": "/home/static/media/logoAssocia.eb722da3.png"
+  }
+]);
